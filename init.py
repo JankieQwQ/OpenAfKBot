36d1ccb88fb4c4eea212a4b686f0ea27277ed19a
JankieQwQ <qwq@awaish.link>
2024-04-21 18:06:03 +0800

    feat: Thread

--- init.py
+++ init.py
@@ -1,4 +1,8 @@
 import os
+import _thread
 
-os.system('start src/woo.py')
-os.system('start src/main.py')
+def startBot():
+    os.system('start src/woo.py')
+    os.system('start src/main.py')
+
+_thread.start_new_thread(startBot,())
