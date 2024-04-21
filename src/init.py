ff75df593e440ca2f702d0a182ebdf1601a83a18
JankieQwQ <qwq@awaish.link>
2024-04-21 18:03:48 +0800

    feat: Update readme

--- src/init.py
+++ src/init.py
@@ -5,16 +5,23 @@ import password
 import hackchat
 import urllib.request
 
+readme = '''
+Command list:
+> help, afk, awa, back, time, peep, ping
+
+Open source in [GitHub](https://github.com/JankieQwQ/OpenAfKBot)
+'''
+
 def message_got(chat, message, sender):
     if ':!help' == message:
-        chat.send_message("Command list:```help,afk,setu,back,time,peep```")
+        chat.send_message("")
     if ':!setu' == message:
         chat.send_message(setu.random_picture())
     if ':!afk' == message:
         chat.send_message("@{} You're AFK now.".format(sender))
     if ':!back' == message:
         chat.send_message("@{} Welcome back.".format(sender))
-    if ':!peep' == message:
+    if ':!awa' == message:
         try:
             message_list = str(urllib.request.urlopen('http://127.0.0.1:2323/message').read())
         except Exception as e:
