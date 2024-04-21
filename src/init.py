4d3d921ab225497c7ed5d2a62c49c6e923dbfb85
JankieQwQ <qwq@awaish.link>
2024-04-21 12:43:37 +0800

    feat: AFK

--- src/init.py
+++ src/init.py
@@ -7,6 +7,11 @@ def message_got(chat, message, sender):
         chat.send_message("Command list:```help,afk,setu,back,time,lookup```")
     if ':!setu' == message:
         chat.send_message(setu.random_picture())
+    if ':!afk' == message:
+        chat.send_message("@{} You're AFK now.".format(sender))
+    if ':!back' == message:
+        chat.send_message("@{} Welcome back.".format(sender))
+
 chat = hackchat.HackChat(password.username "lounge")
 chat.on_message += [message_got]
 chat.run()
